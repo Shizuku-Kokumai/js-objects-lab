ac382bedd4de9417328d1e87e5e57a76da858ad7
Shizuku Kokumai <bp20005@shibaura-it.ac.jp>
2022-10-03 02:29:51 +0900

    Dynamic properties

--- fundamentals.js
+++ fundamentals.js
@@ -3,9 +3,19 @@ const aCar = {
     type: {
         make: "Toyota",
         model: "Corolla",
-        cc: "1.8"
+        cc: 1.8
     },
     registration: '201WD1058'
 };
 
-console.log(aCar.owner+'drives a '+aCar.type.make);
+aCar.milage = 10000;
+aCar.color = {
+    exterior: "red",
+    interior: {
+        texture: "leather",
+        shade: "cream"
+    }
+};
+
+console.log(aCar.owner + 'drives a ' + aCar.type.make);
+console.log('It is a ' + aCar.color.exterior + 'color,' + aCar.milage + ' milage, with ' + aCar.color.interior.texture + 'interior.');
